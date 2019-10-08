b567c0ae10aad8f89bcecedd7b0668ce5cd73f6b
Bektursun <samatuulu11@gmail.com>
2019-10-08 18:45:16 +0600

    Created model Project with some fields. Registered in admin.py. Updated fixtures with projet model.

--- source/issuetracker/admin.py
+++ source/issuetracker/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Task, Type, Status
+from .models import Task, Type, Status, Project
 
 
 class TaskAdmin(admin.ModelAdmin):
@@ -7,10 +7,11 @@ class TaskAdmin(admin.ModelAdmin):
     list_filter = ['summary']
     list_display_links = ['pk', 'summary']
     search_fields = ['summary', 'description']
-    fields = ['summary', 'description', 'status', 'type', 'created_at']
+    fields = ['summary', 'description', 'status', 'type', 'project', 'created_at']
     readonly_fields = ['created_at']
 
 
 admin.site.register(Task, TaskAdmin)
 admin.site.register(Type)
 admin.site.register(Status)
+admin.site.register(Project)
--- source/issuetracker/models.py
+++ source/issuetracker/models.py
@@ -6,6 +6,7 @@ class Task(models.Model):
     description = models.TextField(max_length=2000, null=True, blank=True)
     status = models.ForeignKey('issuetracker.Status', related_name='task_status', on_delete=models.PROTECT, verbose_name='Status')
     type = models.ForeignKey('issuetracker.Type', related_name='task_type', on_delete=models.PROTECT, verbose_name='Type')
+    project = models.ForeignKey('issuetracker.Project', related_name='task_project', on_delete=models.PROTECT, null=True, blank=False)
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Created at')
 
     def __str__(self):
@@ -24,3 +25,13 @@ class Status(models.Model):
 
     def __str__(self):
         return self.status
+
+
+class Project(models.Model):
+    tittle = models.CharField(max_length=50, null=False, blank=False, verbose_name='Title')
+    description = models.TextField(max_length=2000, null=True, blank=True, verbose_name='Description')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Created at')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Updated at')
+
+    def __str__(self):
+        return self.tittle

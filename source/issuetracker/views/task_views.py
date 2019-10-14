a890b0f9b430e3c0da79a799f5653feff740facd
Bektursun <samatuulu11@gmail.com>
2019-10-14 14:41:00 +0600

    Now same thing but with Tasks. Task are available to search with some letter or with full name.

--- source/issuetracker/views/task_views.py
+++ source/issuetracker/views/task_views.py
@@ -1,18 +1,47 @@
+from urllib.parse import urlencode
+
+from django.db.models import Q
 from django.urls import reverse, reverse_lazy
 from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
 
-from issuetracker.forms import TaskForm
+from issuetracker.forms import TaskForm, SimpleSearchForm
 from issuetracker.models import Task
 
 
 class IndexView(ListView):
     template_name = 'task/index.html'
     context_object_name = 'tasks'
+    model = Task
     paginate_by = 2
     paginate_orphans = 1
 
+    def get(self, request, *args, **kwargs):
+        self.form = self.get_search_form()
+        self.search_value = self.get_search_value()
+        return super().get(request, *args, **kwargs)
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(object_list=object_list, **kwargs)
+        context['form'] = self.form
+
+        if self.search_value:
+            context['query'] = urlencode({'search': self.search_value})
+        return context
+
     def get_queryset(self):
-        return Task.objects.all().order_by('-created_at')
+        queryset = super().get_queryset()
+        if self.search_value:
+            query = Q(summary__icontains=self.search_value) | Q(description__icontains=self.search_value)
+            queryset = queryset.filter(query)
+        return queryset
+
+    def get_search_form(self):
+        return SimpleSearchForm(self.request.GET)
+
+    def get_search_value(self):
+        if self.form.is_valid():
+            return self.form.cleaned_data['search']
+        return None
 
 
 class TaskCreateView(CreateView):

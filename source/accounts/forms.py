e2e71bde977172cdf561cc2cd0d93fcc33c374fa
Bektursun <samatuulu11@gmail.com>
2019-10-28 17:48:47 +0600

    Refactoring... worked with some fields in UsercreationForm and worked with design of registration form and login form. Worked with functionality of registration. Done task.

--- source/accounts/forms.py
+++ source/accounts/forms.py
@@ -1,20 +1,50 @@
 from django.contrib.auth.models import User
 from django import forms
+from django.core.exceptions import ValidationError
 
 
 class UserCreationForm(forms.ModelForm):
-    password = forms.CharField(label='Password', strip=False, widget=forms.PasswordInput)
-    password_confirm = forms.CharField(label='Confirm password', widget=forms.PasswordInput, strip=False)
-    first_name = forms.CharField(label='Name', max_length=100, required=False)
-    last_name = forms.CharField(label='Last name', max_length=100, required=False)
-    email = forms.CharField(label='Email', required=True)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['username'].widget.attrs.update({'class': 'form-control'})
+        self.fields['password'].widget.attrs.update({'class': 'form-control'})
+        self.fields['password_confirm'].widget.attrs.update({'class': 'form-control'})
+        self.fields['first_name'].widget.attrs.update({'class': 'form-control'})
+        self.fields['last_name'].widget.attrs.update({'class': 'form-control'})
+        self.fields['email'].widget.attrs.update({'class': 'form-control'})
+
+    username = forms.CharField(max_length=50, label='Username', required=True)
+    password = forms.CharField(label='Password:', strip=False, widget=forms.PasswordInput)
+    password_confirm = forms.CharField(label='Confirm password:', widget=forms.PasswordInput, strip=False)
+    first_name = forms.CharField(label='First name:', max_length=100, required=False)
+    last_name = forms.CharField(label='Last name:', max_length=100, required=False)
+    email = forms.CharField(label='Email:', required=True)
+
+    def clean_username(self):
+        username = self.cleaned_data.get('username')
+        try:
+            User.objects.get(username=username)
+            raise ValidationError('User with this username already exists!',
+                                  code='user_username_exists')
+        except User.DoesNotExist:
+            return username
 
     def clean(self):
         first_name = self.cleaned_data.get('first_name')
         last_name = self.cleaned_data.get('last_name')
 
         if not (first_name or last_name):
-            raise forms.ValidationError('First name or last name needs to fill out.')
+            raise forms.ValidationError('First name or last name needs to fill out!')
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        try:
+            User.objects.get(email=email)
+            raise ValidationError('User with this email already exists!',
+                                  code='user_email_exists')
+        except User.DoesNotExist:
+            return email
 
     def clean_password_confirm(self):
         password = self.cleaned_data.get("password")

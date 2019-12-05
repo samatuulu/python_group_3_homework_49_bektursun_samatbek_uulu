d246ec1cfdd945d08be2ab84840b40f3278c70cb
Bektursun <samatuulu11@gmail.com>
2019-12-05 15:55:27 +0600

    Project and Tasks ara able to get from API

--- source/api/serializers.py
+++ source/api/serializers.py
@@ -3,12 +3,6 @@ from rest_framework import serializers
 from issuetracker.models import Project, Task
 
 
-class ProjectSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Project
-        fields = ('id', 'tittle', 'description', 'created_at', 'updated_at', 'status')
-
-
 class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
@@ -17,3 +11,11 @@ class TaskSerializer(serializers.ModelSerializer):
             'project', 'created_at',
             'created_by', 'assigned_to'
         )
+
+
+class ProjectSerializer(serializers.ModelSerializer):
+    task_project = TaskSerializer(many=True, read_only=True)
+    class Meta:
+        model = Project
+        fields = ('id', 'tittle', 'description', 'created_at', 'updated_at', 'status', 'task_project')
+

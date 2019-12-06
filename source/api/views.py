0b8726019c8092a30df517e55b63992b59a8ffba
Bektursun <samatuulu11@gmail.com>
2019-12-06 18:27:17 +0600

    Done first task

--- source/api/views.py
+++ source/api/views.py
@@ -1,25 +1,28 @@
 from rest_framework import viewsets
-from rest_framework.permissions import SAFE_METHODS
+from rest_framework.permissions import SAFE_METHODS, IsAdminUser, IsAuthenticated, AllowAny, DjangoModelPermissions, \
+    DjangoModelPermissionsOrAnonReadOnly
 
 from issuetracker.models import Project, Task
 from api.serializers import ProjectSerializer, TaskSerializer
 
 
 class ProjectViewSet(viewsets.ModelViewSet):
+    permission_classes = [DjangoModelPermissionsOrAnonReadOnly]
     queryset = Project.objects.all()
     serializer_class = ProjectSerializer
 
-    def get_permissions(self):
-        if self.request.method in SAFE_METHODS:
-            return []
-        return super().get_permissions()
+    # def get_permissions(self):
+    #     if self.request.method in SAFE_METHODS:
+    #         return []
+    #     return super().get_permissions()
 
 
 class TaskViewSet(viewsets.ModelViewSet):
+    permission_classes = [DjangoModelPermissionsOrAnonReadOnly]
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
 
-    def get_permissions(self):
-        if self.request.method in SAFE_METHODS:
-            return []
-        return super().get_permissions()
+    # def get_permissions(self):
+    #     if self.request.method in SAFE_METHODS:
+    #         return []
+    #     return super().get_permissions()

409f30a80a81190e4309ce3b671e4c46be3da5ab
Bektursun <samatuulu11@gmail.com>
2019-12-06 18:06:47 +0600

    Project and Task on read mode in API.

--- source/api/views.py
+++ source/api/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework.permissions import SAFE_METHODS
 
 from issuetracker.models import Project, Task
 from api.serializers import ProjectSerializer, TaskSerializer
@@ -8,7 +9,17 @@ class ProjectViewSet(viewsets.ModelViewSet):
     queryset = Project.objects.all()
     serializer_class = ProjectSerializer
 
+    def get_permissions(self):
+        if self.request.method in SAFE_METHODS:
+            return []
+        return super().get_permissions()
+
 
 class TaskViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
+
+    def get_permissions(self):
+        if self.request.method in SAFE_METHODS:
+            return []
+        return super().get_permissions()
--- source/issuetracker/static/js/task_2.js
+++ source/issuetracker/static/js/task_2.js
@@ -10,3 +10,15 @@ $.ajax({
         },
     error: function(response, status){console.log(response);}
 });
+
+
+$.ajax({
+     url: 'http://localhost:8000/api/project/',
+     method: 'get',
+     headers: {'Authorization': 'Token ' + localStorage.getItem('apiToken')},
+     data: JSON.stringify({username: 'admin', password: 'admin'}),
+     dataType: 'json',
+     contentType: 'application/json',
+     success: function(response, status){console.log(response);},
+     error: function(response, status){console.log(response);}
+});
